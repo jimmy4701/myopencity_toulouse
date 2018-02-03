7312cf90d327f3092fb097dff54d345d35e72190
Jérémy SIMONKLEIN ANGÉLY <jeremy.simonklein@gmail.com>
2018-02-03 14:50:32 +0100

    Added territory attribute on consults

--- imports/api/territories/server/publication.js
+++ imports/api/territories/server/publication.js
@@ -3,13 +3,22 @@ import { Territories } from '../territories'
 
 Meteor.publish('territories.all', function () {
     if (Roles.userIsInRole(this.userId, ['admin', 'moderator'])) {
-        return Territories.find({})
+        return Territories.find({}, {limit: 1000, sort: {}})
     }
 })
 
 Meteor.publish('territories.by_id', function (id) {
     if (Roles.userIsInRole(this.userId, ['admin', 'moderator'])) {
-        return Territories.find({ _id: id })
+        return Territories.find({ _id: id }, {limit: 1000, sort: {}})
+    }
+})
+
+Meteor.publish('territories.authorized_for_me', function() {
+    const user = Meteor.user()
+    if(Roles.userIsInRole(this.userId, 'admin')){
+        return Territories.find({}, {limit: 1000, sort: {}})
+    }else{
+        return Territories.find({shorten_url: {$in: user.roles }}, {limit: 1000, sort: {}})
     }
 })
 
