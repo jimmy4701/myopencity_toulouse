55075e05a377c6824e861131b07381ec6536cc66
Jimmy <jeremy.simonklein@gmail.com>
2018-03-18 17:22:20 +0100

    Replaced anonymous button by checkbox for AlternativeForm

--- imports/components/alternatives/AlternativeForm.jsx
+++ imports/components/alternatives/AlternativeForm.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import TinyMCE from 'react-tinymce'
-import {Grid, Header, Form, Input, Button, Icon} from 'semantic-ui-react'
+import {Grid, Header, Form, Input, Button, Icon, Checkbox} from 'semantic-ui-react'
 
 export default class AlternativeForm extends Component{
 
@@ -90,17 +90,14 @@ export default class AlternativeForm extends Component{
                    plugins: 'image',
                    toolbar: false,
                    menubar: false,
-                   branding: false
+                   branding: false,
+                   statusbar: false
                  }}
                  onChange={this.handleContentChange.bind(this)}
                 />
              </Form.Field>
              <Form.Field>
-               {alternatives_anonymous_choice ?
-                 <Button onClick={(e) => {this.toggleAlternative('anonymous', e)}} icon="spy" content={alternative.anonymous ?
-                     "Votre nom ne sera pas affiché"
-                     : 'Votre nom sera affiché'} />
-               : ''} 
+                <Checkbox style={{margin: "1em 0"}} checked={alternative.anonymous} onClick={(e) => {this.toggleAlternative('anonymous', e)}} label="Je souhaite rester anonyme"/> <br/>
                <Button disabled={!this.isValid()} positive onClick={(e) => {this.submit_form(e)}}>{consult_alternative_validation_term}</Button>
              </Form.Field>
            </Form>
