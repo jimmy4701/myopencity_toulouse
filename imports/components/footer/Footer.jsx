1c27286b43912d5b0781c31d467f53318c3c62cf
Jimmy <jeremy.simonklein@gmail.com>
2018-03-25 13:03:19 +0100

    Modified footer

--- imports/components/footer/Footer.jsx
+++ imports/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Container, Image } from 'semantic-ui-react';
+import { Grid, Container, Image, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 export default class Footer extends Component {
@@ -11,20 +11,29 @@ export default class Footer extends Component {
                 <Grid.Column width={16}>
                     <Container>
                         <Grid stackable centered>
-                            <Grid.Column width={16} className="center-align">
-                                <Link to="//toulouse.fr" target="_blank">
+                        <Grid.Column width={3}>
+                            
+                        </Grid.Column>
+                            <Grid.Column width={10} className="center-align">
+                                <Link to="http://toulouse.fr" target="_blank">
                                     <Image className="inline-block footer-image" src="/images/mairie-toulouse.png" inline fluid size="small"/>
                                 </Link>
                                 <Link to="//toulouse-metropole.fr" target="_blank">
                                     <Image className="inline-block footer-image" src="/images/toulouse-metropole.png" inline fluid size="small"/>
                                 </Link>
+                                <br/>
+                                <div style={{margin: "1em 0"}}>{footer_content}</div>
                             </Grid.Column>
-                            <Grid.Column width={16} className="center-align" style={{padding: "0"}}>
-                                <span>{footer_content}</span>
-                            </Grid.Column>
-                            <Grid.Column width={16} className="center-align" style={{padding: "0"}}>
-                                {footer_cgu_display && <Link to='/conditions' className="pointer footer-link" ><span style={{color: footer_color}}>{cgu_term}</span></Link>}
-                                {footer_legal_notice_display && <Link to='/mentions_legales' className="pointer footer-link" ><span style={{color: footer_color}}>{legal_notice_term}</span></Link>}
+                            <Grid.Column width={3} verticalAlign="middle" style={{textAlign: "right"}}>
+                                {footer_cgu_display && <Link to='/conditions' className="pointer footer-link" ><span style={{color: footer_color}}>{cgu_term}<br/></span></Link>}
+                                {footer_legal_notice_display && <Link to='/mentions_legales' className="pointer footer-link" ><span style={{color: footer_color}}>{legal_notice_term}<br/></span></Link>}
+                                <Link to="//facebook.com/Toulouse" target="_blank">
+                                    <Icon name="facebook" circular style={{
+                                        backgroundColor: "#c5c5c5",
+                                        boxShadow: "none",
+                                        color: "white"
+                                    }}/>
+                                </Link>
                             </Grid.Column>
                         </Grid>
                     </Container>
