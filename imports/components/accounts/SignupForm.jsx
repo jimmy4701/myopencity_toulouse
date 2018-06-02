a52d356101268d7caae51c1ea306901b1e7a8f0e
Jérémy SIMONKLEIN ANGELY <jeremy.simonklein@gmail.com>
2018-06-02 20:19:47 +0200

    Modified signup form to have two steps

--- imports/components/accounts/SignupForm.jsx
+++ imports/components/accounts/SignupForm.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
-import {Form, Input, Button, Divider, Checkbox} from 'semantic-ui-react'
+import {Form, Input, Button, Divider, Checkbox, Header} from 'semantic-ui-react'
 import {Meteor} from 'meteor/meteor'
-import { createContainer } from 'meteor/react-meteor-data'
+import { withTracker } from 'meteor/react-meteor-data'
 import {Configuration} from '/imports/api/configuration/configuration'
 import {Link, withRouter} from 'react-router-dom'
 import ReCAPTCHA from "react-google-recaptcha"
+import styled from 'styled-components'
+import { Territories } from '/imports/api/territories/territories'
 
 export class SignupForm extends Component{
 
@@ -14,7 +16,9 @@ export class SignupForm extends Component{
 
     state = {
       user: {},
-      captcha: ""
+      captcha: "",
+      step: "account",
+      profile: {}
     }
 
   handleChange(attr, e){
@@ -46,13 +50,10 @@ export class SignupForm extends Component{
             })
           }else{
             const {email, password} = user
-            Meteor.loginWithPassword(email, password)
-            const return_route = Session.get('return_route')
-            if(return_route){
-              this.props.history.push(return_route)
-            }else{
-              this.props.history.push('/consults')
-            }
+            Meteor.loginWithPassword(email, password, () => {
+              this.setState({step: "profile"})
+            })
+            
             Bert.alert({
               title: "Votre compte a bien été créé",
               type: 'success',
@@ -71,12 +72,7 @@ export class SignupForm extends Component{
       if(error){
         console.log("Error during facebook login", error)
       }else{
-        const return_route = Session.get('return_route')
-        if(return_route){
-          this.props.history.push(return_route)
-        }else{
-          this.props.history.push('/consults')
-        }
+        this.setState({step: "profile"})
       }
     })
   }
@@ -87,110 +83,275 @@ export class SignupForm extends Component{
       if(error){
         console.log("Error during google login", error)
       }else{
+        this.setState({step: "profile"})
+      }
+    })
+  }
+
+  editProfile = (e) => {
+    e.preventDefault()
+    const {profile} = this.state
+    Meteor.call('user.edit_profile', profile , (error, result) => {
+      if(error){
+        console.log('Erreur', error.message)
+      }else{
         const return_route = Session.get('return_route')
         if(return_route){
           this.props.history.push(return_route)
         }else{
           this.props.history.push('/consults')
         }
+        Bert.alert({
+          title: 'Vous êtes maintenant inscrit',
+          style: 'growl-bottom-left',
+          type: 'success'
+        })
       }
     })
-  }
+  } 
 
   handleCaptcha = (captcha) => this.setState({captcha})
 
+  handleHomeChange = (event, data) => {
+    let {profile} = this.state
+    profile.home_territories = data.value
+    this.setState({profile})
+  }
+
+  handleWorkChange = (event, data) => {
+    let {profile} = this.state
+    profile.work_territories = data.value
+    this.setState({profile})
+  }
+
+  handleInterestChange = (event, data) => {
+    let {profile} = this.state
+    profile.interest_territories = data.value
+    this.setState({profile})
+  }
+
+  handleAge = (event, data) => {
+    let {profile} = this.state
+    profile.age = data.value
+    this.setState({profile})
+  }
+
+  handleGender = (event, data) => {
+    let {profile} = this.state
+    profile.gender = data.value
+    this.setState({profile})
+  }
+
 
   render(){
-    const {user, accept_conditions, accept_legal_notice, error_message} = this.state
-    const {global_configuration, loading} = this.props
-    const {facebook_connected, google_connected, cgu_term, cgu_acceptance, legal_notice_term, legal_notice_acceptance, cnil_signup_text} = global_configuration
+    const {user, accept_conditions, accept_legal_notice, error_message, step, profile} = this.state
+    const {global_configuration, loading, className, territories} = this.props
+    const {facebook_connected, google_connected, cgu_term, cgu_acceptance, legal_notice_term, legal_notice_acceptance, cnil_signup_text, navbar_color} = global_configuration
     const isValid = user.email && user.password && user.username && user.password == user.confirm_password && (cgu_acceptance ? accept_conditions : true) && (legal_notice_acceptance ? accept_legal_notice : true)
 
     if(!loading){
-      return(
-        <Form>
-          <Form.Field required>
-            <label>Nom ou pseudo</label>
-            <Input fluid type="text" onChange={(e) => {this.handleChange('username', e)}} />
-          </Form.Field>
-          <Form.Field required>
-            <label>Votre adresse email</label>
-            <Input fluid type="email" onChange={(e) => {this.handleChange('email', e)}} />
-          </Form.Field>
-          <Form.Field required>
-            <label>Mot de passe</label>
-            <Input fluid type="password" onChange={(e) => {this.handleChange('password', e)}} />
-          </Form.Field>
-          <Form.Field required>
-            <label>Confirmez votre mot de passe</label>
-            <Input fluid type="password" onChange={(e) => {this.handleChange('confirm_password', e)}} />
-            {this.state.confirm_password && (this.state.password != this.state.confirm_password) ?
-              <p>Le mot de passe et la confirmation ne sont pas identiques</p>
-            : ''}
-          </Form.Field>
-          {cgu_acceptance &&
-            <Form.Field>
-              <Checkbox
-                required
-                checked={accept_conditions}
-                onClick={() => this.toggleState('accept_conditions')}
-                label={<label for="accept_conditions"  >J'accepte les conditions de la <a href="/conditions" target="_blank">charte d'utilisation</a><span className="required-char"> *</span></label>}
-              />
-            </Form.Field>
-          }
 
-          {legal_notice_acceptance &&
-            <Form.Field>
-              <Checkbox
-                required
-                checked={accept_legal_notice}
-                onClick={() => this.toggleState('accept_legal_notice')}
-                label={<label for="accept_legal_notice"  >J'accepte les <a href="/mentions_legales" target="_blank">mentions légales</a><span className="required-char"> *</span></label>}
-              />
-            </Form.Field>
-          }
-          <Form.Field>
-            <ReCAPTCHA
-              ref="recaptcha"
-              sitekey="6Lf1g0wUAAAAAEvKqeT6sWNCvRgB4Cxbv2tqvhSo"
-              onChange={this.handleCaptcha}
-              size="invisible"
-              badge="inline"
-            />
-            </Form.Field>
-          <Button onClick={this.create_account}>M'inscrire</Button>
-          {(error_message && !isValid) && <div><label>Les données du formulaire ne sont pas valides</label></div> }
-          <p style={{fontSize: "0.7em"}}><span style={{color: "red"}}>*</span>Champs obligatoires</p>
-          {facebook_connected || google_connected ?
-            <Divider horizontal>OU</Divider>
-          : ''}
-          {facebook_connected ?
-            <Button color="blue" icon="facebook" content="Se connecter avec Facebook" onClick={(e) => {this.connect_facebook(e)}}/>
-          : ''}
-          {google_connected ?
-            <Button color="red" icon="google" content="Se connecter avec Google" onClick={(e) => {this.connect_google(e)}}/>
-          : ''}
-          {cnil_signup_text &&
-            <div>
-              <Divider />
-              <div className="cnil-signup-text dangerous" dangerouslySetInnerHTML={{__html: cnil_signup_text }} />
-            </div>
-          }
-          
-        </Form>
-      )
+      const territories_options = territories.map(territory => {
+        return {
+          key: territory._id,
+          value: territory._id,
+          text: territory.name
+        }
+      })
+
+      const age_options = [
+        {
+          key: "18",
+          value: "18",
+          text: "Moins de 18 ans"
+        },
+        {
+          key: "24",
+          value: "24",
+          text: "Entre 18 et 24 ans"
+        },
+        {
+          key: "39",
+          value: "39",
+          text: "Entre 25 et 39 ans"
+        },
+        {
+          key: "65",
+          value: "65",
+          text: "Entre 40 et 65 ans"
+        },
+        {
+          key: "80",
+          value: "80",
+          text: "Plus de 65 ans"
+        }
+      ]
+        return(
+          <Form className={className}>
+            {step == "account" &&
+              <div>
+                <Form.Field required>
+                  <label>Nom ou pseudo</label>
+                  <Input fluid type="text" onChange={(e) => {this.handleChange('username', e)}} />
+                </Form.Field>
+                <Form.Field required>
+                  <label>Votre adresse email</label>
+                  <Input fluid type="email" onChange={(e) => {this.handleChange('email', e)}} />
+                </Form.Field>
+                <Form.Field required>
+                  <label>Mot de passe</label>
+                  <Input fluid type="password" onChange={(e) => {this.handleChange('password', e)}} />
+                </Form.Field>
+                <Form.Field required>
+                  <label>Confirmez votre mot de passe</label>
+                  <Input fluid type="password" onChange={(e) => {this.handleChange('confirm_password', e)}} />
+                  {this.state.confirm_password && (this.state.password != this.state.confirm_password) ?
+                    <p>Le mot de passe et la confirmation ne sont pas identiques</p>
+                  : ''}
+                </Form.Field>
+                {cgu_acceptance &&
+                  <Form.Field>
+                    <Checkbox
+                      required
+                      checked={accept_conditions}
+                      onClick={() => this.toggleState('accept_conditions')}
+                      label={<label for="accept_conditions"  >J'accepte les conditions de la <a href="/conditions" target="_blank">charte d'utilisation</a><span className="required-char"> *</span></label>}
+                    />
+                  </Form.Field>
+                }
+      
+                {legal_notice_acceptance &&
+                  <Form.Field>
+                    <Checkbox
+                      required
+                      checked={accept_legal_notice}
+                      onClick={() => this.toggleState('accept_legal_notice')}
+                      label={<label for="accept_legal_notice"  >J'accepte les <a href="/mentions_legales" target="_blank">mentions légales</a><span className="required-char"> *</span></label>}
+                    />
+                  </Form.Field>
+                }
+                <Form.Field>
+                  <ReCAPTCHA
+                    ref="recaptcha"
+                    sitekey="6Lf1g0wUAAAAAEvKqeT6sWNCvRgB4Cxbv2tqvhSo"
+                    onChange={this.handleCaptcha}
+                    size="invisible"
+                    badge="inline"
+                  />
+                  </Form.Field>
+                  <div className="submit-buttons">
+                    <Button className="submit-button" style={{backgroundColor: navbar_color}} onClick={this.create_account}>Suivant ></Button>
+                    {(error_message && !isValid) && <div><label>Les données du formulaire ne sont pas valides</label></div> }
+                    <p style={{fontSize: "0.7em"}}><span style={{color: "red"}}>*</span>Champs obligatoires</p>
+                    {facebook_connected || google_connected ?
+                      <Divider horizontal>OU</Divider>
+                    : ''}
+                    {facebook_connected ?
+                      <Button color="blue" icon="facebook" content="Se connecter avec Facebook" onClick={(e) => {this.connect_facebook(e)}}/>
+                    : ''}
+                    {google_connected ?
+                      <Button color="red" icon="google" content="Se connecter avec Google" onClick={(e) => {this.connect_google(e)}}/>
+                    : ''}
+                  </div>
+              </div>
+            }
+            {step == "profile" &&
+                <div>
+                  <Header as='h3'>Vous et Toulouse</Header>
+                  <Header as='h4'>Aidez nous à mieux vous connaître</Header>
+                  <Form.Group widths="equal">
+                      <Form.Select
+                        label="Votre âge"
+                        options={age_options}
+                        value={profile.age}
+                        onChange={this.handleAge}
+                        name="age"/>
+                      <Form.Select
+                        label="Votre genre"
+                        options={[
+                        {
+                          key: "man",
+                          value: "man",
+                          text: "Un homme"
+                        }, {
+                          key: "woman",
+                          value: "woman",
+                          text: "Une femme"
+                        }, {
+                          key: "other",
+                          value: "other",
+                          text: "Autre"
+                        }
+                      ]}
+                        value={profile.gender}
+                        onChange={this.handleGender}
+                        name="gender"/>
+                  </Form.Group>
+                  <Form.Select
+                    label='Dans quel quartier habitez-vous ?'
+                    onChange={this.handleHomeChange}
+                    value={profile.home_territories}
+                    name='home_territories'
+                    options={territories_options}
+                  />
+                  <Form.Select
+                    label='Dans quel quartier travaillez-vous ?'
+                    onChange={this.handleWorkChange}
+                    value={profile.work_territories}
+                    name='work_territories'
+                    options={territories_options}
+                  />
+                  <Form.Select
+                    label='Quels quartiers vous intéressent ?'
+                    onChange={this.handleInterestChange}
+                    value={profile.interest_territories}
+                    name='interest_territories'
+                    options={territories_options}
+                    multiple
+                  />
+                  <div style={{textAlign: "center"}}>
+                    <Button className="submit-button" style={{backgroundColor: navbar_color}} onClick={this.editProfile}>Finaliser mon inscription</Button>
+                  </div>
+                </div>
+            }
+              {cnil_signup_text &&
+                <div>
+                  <Divider />
+                  <div className="cnil-signup-text dangerous" dangerouslySetInnerHTML={{__html: cnil_signup_text }} />
+                </div>
+              }
+            
+          </Form>
+        )
+
     }else{
       return <div></div>
     }
   }
 }
 
-export default SignupFormContainer = createContainer(() => {
+export default SignupFormContainer = withTracker(() => {
   const globalConfigurationPublication = Meteor.isClient && Meteor.subscribe('global_configuration')
-  const loading = Meteor.isClient && !globalConfigurationPublication.ready()
+  const territoriesPublication = Meteor.isClient && Meteor.subscribe('territories.active')
+  const loading = Meteor.isClient && (!globalConfigurationPublication.ready() || !territoriesPublication.ready())
   const global_configuration = Configuration.findOne({})
+  const territories = Territories.find({active: true}).fetch()
   return {
     loading,
-    global_configuration
+    global_configuration,
+    territories
   }
-}, withRouter(SignupForm))
+})(withRouter(styled(SignupForm)`
+  > div .submit-buttons{
+    text-align: center;
+
+    > .submit-button {
+      padding: 1em 4em;
+      color: white;
+    }
+  }
+  > div .submit-button {
+    padding: 1em 4em;
+    color: white;
+  }
+`))
--- imports/pages/accounts/SignupPage.jsx
+++ imports/pages/accounts/SignupPage.jsx
@@ -2,33 +2,29 @@ import React, { Component } from 'react'
 import TrackerReact from 'meteor/ultimatejs:tracker-react'
 import { Grid, Header, Container, Image } from 'semantic-ui-react'
 import SignupForm from '/imports/components/accounts/SignupForm'
+import styled from 'styled-components'
 
-export default class SignupPage extends TrackerReact(Component) {
+class SignupPage extends Component {
 
   /*
     required params:
   */
 
-  constructor(props) {
-    super(props);
-    this.state = {
+  state = {
 
-    }
   }
 
   render() {
     const { global_image_url } = Meteor.isClient && Session.get('global_configuration')
+    const { className } = this.props
 
     return (
-      <Grid stackable>
+      <Grid stackable className={className}>
         <Grid.Column width={16} className="mobile-padding">
           <Container>
             <Grid stackable centered className="wow fadeInUp main-container" verticalAlign="middle">
-              <Grid.Column width={8} className="center-align">
-                <Image src={global_image_url} inline size="medium" className="wow fadeInUp" />
-              </Grid.Column>
-              <Grid.Column width={8} className="mobile-padding">
-                <Header as="h1">Inscription</Header>
+              <Grid.Column width={16} className="mobile-padding">
+                <Header as="h1"><Image className="image" src={global_image_url} size="small" /> Inscription</Header>
                 <SignupForm />
               </Grid.Column>
             </Grid>
@@ -38,3 +34,13 @@ export default class SignupPage extends TrackerReact(Component) {
     )
   }
 }
+
+export default styled(SignupPage)`
+  > div div .main-container {
+    padding: 0 14em !important;
+  }
+
+  > div div div div .image {
+    width: 5em !important;
+  }
+`
