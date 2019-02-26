2b1807869756048a7ada74e9d6f73c785a153c48
Jimmy <jeremy.simonklein@gmail.com>
2019-02-26 10:50:23 +0100

    Added BudgetConsult page

--- imports/pages/budget_consults/BudgetConsult.js
+++ imports/pages/budget_consults/BudgetConsult.js
@@ -2,19 +2,48 @@ import React, {Component} from 'react'
 import styled from 'styled-components'
 import { withTracker } from 'meteor/react-meteor-data'
 import { BudgetConsults } from '/imports/api/budget_consults/budget_consults'
+import {Helmet} from 'react-helmet'
 
 class BudgetConsult extends Component {
     state = {
         
     }
 
+    componentDidMount(){
+        window.scrollTo({top: 0, behavior: "smooth"})
+    }
+
     render(){
         const { loading, budget_consult } = this.props
+
+        const {
+            consult_header_height,
+            consult_header_color,
+            consult_description_background_color,
+            consult_description_color,
+            consult_description_font_size,
+            consult_territory_prefix, 
+            consult_territory_icon,
+            buttons_validation_text_color,
+            buttons_validation_background_color,
+            consult_term
+          } = Meteor.isClient && Session.get('global_configuration')
 
         if(!loading){
             return(
                 <MainContainer>
-                    <h3>{budget_consult.title}</h3>
+                    <Helmet>
+                        <meta property="og:title" content={budget_consult.title} />
+                        <meta property="og:type" content="article" />
+                        <meta property="og:description" content={budget_consult.description} />
+                        <meta name="description" content={budget_consult.description} />
+                        <meta property="og:image" content={budget_consult.image_url} />
+                        <meta property="og:url" content={"https://jeparticipe.toulouse.fr" + this.props.location.pathname} />
+                    </Helmet>
+                    <ConsultHeader height={consult_header_height} image_url={budget_consult.image_url}>
+                        <ConsultBackground/>
+                        <ConsultTitle className="animated fadeInUp" color={consult_header_color}>{budget_consult.title}</ConsultTitle>
+                    </ConsultHeader>
                 </MainContainer>
             )
         }else{
@@ -36,4 +65,29 @@ export default BudgetConsultContainer = withTracker(({match}) => {
 
 const MainContainer = styled.div`
     
+`
+
+const ConsultHeader = styled.div`
+    background-image: url('${props => props.image_url}');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    height: ${props => props.height};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    margin-top: -1em;
+`
+
+const ConsultBackground = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.5);
+    position: absolute;
+`
+
+const ConsultTitle = styled.h3`
+    color: ${props => props.color};
 `
