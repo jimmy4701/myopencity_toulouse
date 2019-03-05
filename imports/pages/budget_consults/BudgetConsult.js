9e3d8ac05897adac22ae77af1f0de72d8bbfd96b
Jimmy <jeremy.simonklein@gmail.com>
2019-03-05 16:03:21 +0100

    Added fragment to budget consult

--- imports/pages/budget_consults/BudgetConsult.js
+++ imports/pages/budget_consults/BudgetConsult.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react'
 import styled from 'styled-components'
 import { withTracker } from 'meteor/react-meteor-data'
 import { BudgetConsults } from '/imports/api/budget_consults/budget_consults'
